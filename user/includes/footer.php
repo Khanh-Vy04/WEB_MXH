f2b86c7f45e76eb725caa6be7320c1c508304646
TuyetTrinh1111 <23521661@gm.uit.edu.vn>
2025-06-12 19:18:15 +0700

    fix foooter

--- user/includes/footer.php
+++ user/includes/footer.php
@@ -6,14 +6,27 @@
 				<div class=" col-md-3 col-sm-6 col-xs-12">
 					<div class="hm-footer-widget">
 						<div class="hm-foot-title">
+							<h4>AuraDisc</h4>
+						</div><!--/.hm-foot-title-->
+						<div class="hm-foot-menu">
+							<ul>
+								<p>Không gian dành cho những tâm hồn mê đắm âm thanh analog, nơi những chiếc đĩa than kể lại câu chuyện âm nhạc đầy chất thơ và hoài niệm. Tại đây, mỗi vòng quay là một hành trình trở về với cảm xúc nguyên bản nhất của giai điệu.</p>
+							</ul><!--/ul-->
+						</div><!--/.hm-foot-menu-->
+					</div><!--/.hm-footer-widget-->
+				</div><!--/.col-->
+				<div class=" col-md-3 col-sm-6 col-xs-12">
+					<div class="hm-footer-widget">
+						<div class="hm-foot-title">
 							<h4>Thông tin</h4>
 						</div><!--/.hm-foot-title-->
 						<div class="hm-foot-menu">
 							<ul>
-								<li><a href="#">Về chúng tôi</a></li><!--/li-->
-								<li><a href="#">Liên hệ</a></li><!--/li-->
-								<li><a href="#">Tin tức</a></li><!--/li-->
-								<li><a href="#">Cửa hàng</a></li><!--/li-->
+								<li><a href="/WEB_MXH/user/index.php">Trang chủ</a></li><!--/li-->
+								<li><a href="/WEB_MXH/user/new-arrivals.php">New Arrivals</a></li><!--/li-->
+								<li><a href="/WEB_MXH/user/Artists/Artists.php">Artists</a></li><!--/li-->
+								<li><a href="/WEB_MXH/user/genre/genres.php">Genres</a></li><!--/li-->
+								<li><a href="/WEB_MXH/user/accessories.php">Accessories</a></li><!--/li-->
 							</ul><!--/ul-->
 						</div><!--/.hm-foot-menu-->
 					</div><!--/.hm-footer-widget-->
@@ -21,31 +34,11 @@
 				<div class=" col-md-3 col-sm-6 col-xs-12">
 					<div class="hm-footer-widget">
 						<div class="hm-foot-title">
-							<h4>Danh mục</h4>
+							<h4>Chính sách</h4>
 						</div><!--/.hm-foot-title-->
 						<div class="hm-foot-menu">
 							<ul>
-								<li><a href="products.php">Sản phẩm</a></li><!--/li-->
-								<li><a href="Artists/Artists.php">Nghệ sĩ</a></li><!--/li-->
-								<li><a href="genre/genres.php">Dòng nhạc</a></li><!--/li-->
-								<li><a href="accessories.php">Phụ kiện</a></li><!--/li-->
-								<li><a href="new-arrivals.php">Hàng mới</a></li><!--/li-->
-							</ul><!--/ul-->
-						</div><!--/.hm-foot-menu-->
-					</div><!--/.hm-footer-widget-->
-				</div><!--/.col-->
-				<div class=" col-md-3 col-sm-6 col-xs-12">
-					<div class="hm-footer-widget">
-						<div class="hm-foot-title">
-							<h4>Tài khoản</h4>
-						</div><!--/.hm-foot-title-->
-						<div class="hm-foot-menu">
-							<ul>
-								<li><a href="profile.php">Tài khoản của tôi</a></li><!--/li-->
-								<li><a href="#">Danh sách yêu thích</a></li><!--/li-->
-								<li><a href="#">Cộng đồng</a></li><!--/li-->
-								<li><a href="#">Lịch sử đơn hàng</a></li><!--/li-->
-								<li><a href="cart.php">Giỏ hàng</a></li><!--/li-->
+								<p>Aura Disc tôn trọng và bảo mật thông tin cá nhân của khách hàng. Sản phẩm được hỗ trợ đổi trả trong vòng 7 ngày nếu có lỗi do vận chuyển hoặc sản xuất. Chúng tôi giao hàng toàn quốc với chi phí và thời gian được thông báo khi xác nhận đơn. Hình thức thanh toán linh hoạt: chuyển khoản, thanh toán khi nhận hàng hoặc qua ví điện tử.</p>
 							</ul><!--/ul-->
 						</div><!--/.hm-foot-menu-->
 					</div><!--/.hm-footer-widget-->
@@ -53,21 +46,19 @@
 				<div class=" col-md-3 col-sm-6  col-xs-12">
 					<div class="hm-footer-widget">
 						<div class="hm-foot-title">
-							<h4>Đăng ký nhận tin</h4>
+							<h4>Liên hệ</h4>
 						</div><!--/.hm-foot-title-->
 						<div class="hm-foot-para">
-							<p>
-								Đăng ký để nhận tin tức, cập nhật và thông tin mới nhất về âm nhạc.
-							</p>
+							<p>Nếu có bất kỳ câu hỏi, nhận xét nào hoặc chỉ để nói về mọi thứ về vinyl, hãy nhớ liên hệ với chúng tôi thông qua (+84)783222894</p>
 						</div><!--/.hm-foot-para-->
-						<div class="hm-foot-email">
+						<!--<div class="hm-foot-email">
 							<div class="foot-email-box">
 								<input type="text" class="form-control" placeholder="Nhập email của bạn...">
-							</div><!--/.foot-email-box-->
+							</div>--><!--/.foot-email-box
 							<div class="foot-email-subscribe">
 								<span><i class="fa fa-location-arrow"></i></span>
-							</div><!--/.foot-email-icon-->
-						</div><!--/.hm-foot-email-->
+							</div>/.foot-email-icon
+						</div>-->
 					</div><!--/.hm-footer-widget-->
 				</div><!--/.col-->
 			</div><!--/.row-->
@@ -82,13 +73,13 @@
 <footer id="footer"  class="footer">
 	<div class="container">
 		<div class="hm-footer-copyright text-center">
-			<div class="footer-social">
+			<!--<div class="footer-social">
 				<a href="#" title="Facebook"><i class="fa fa-facebook"></i></a>	
 				<a href="#" title="Instagram"><i class="fa fa-instagram"></i></a>
 				<a href="#" title="LinkedIn"><i class="fa fa-linkedin"></i></a>
 				<a href="#" title="Pinterest"><i class="fa fa-pinterest"></i></a>
 				<a href="#" title="Behance"><i class="fa fa-behance"></i></a>	
-			</div>
+			</div>-->
 			<p>
 				&copy; 2024 AuraDisc. Tất cả quyền được bảo lưu. Thiết kế và phát triển bởi <a href="#">AuraDisc Team</a>
 			</p><!--/p-->
